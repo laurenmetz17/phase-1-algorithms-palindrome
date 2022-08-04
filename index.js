6ed276fc3b3053a49bfc3b713b066c0e876d69a9
Lauren Metz <Laurenmetz15@gmail.com>
2022-08-04 13:03:14 -0400

    commit tree 9f127fa9ea1128a4f37daacecaa8979f8085e0fd

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //take the string input and return true if that string is a palindrome, false if otherwise
+  for(let i=0; i < word.length;i++) {
+    if(word.charAt(i) == word.charAt(word.length - 1 - i)){
+      console.log(word.charAt(i));
+    }
+    else {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  if string is palandrome (read characters starting from front and back and see tey match to middle)
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+
+  to check if palindrome read characters starting from front and back and see they match to middle)
+  return true if so, false if you reach non matching characaters before the middle of the string
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +38,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecting true')
+  console.log("=>", isPalindrome("ab ba"))
 }
 
 module.exports = isPalindrome;
